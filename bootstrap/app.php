2bfab13f5acf34e002ddef8e1b0b895b773ea2f3
zaayyy <soronganmarcell@gmail.com>
2026-07-31 01:43:50 +0700

    Call useStoragePath on Application instance after ->create()

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__));
-
-if (is_dir('/tmp')) {
-    $app->useStoragePath('/tmp/storage');
-}
-
-return $app
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -36,3 +30,9 @@ return $app
         //
     })
     ->create();
+
+if (is_dir('/tmp')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
